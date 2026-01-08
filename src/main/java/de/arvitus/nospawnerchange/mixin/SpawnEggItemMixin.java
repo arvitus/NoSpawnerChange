6d4a8c498acc2b750ecd6ee6b7a3437318a04733
3D_MAMA <34866314+arvitus@users.noreply.github.com>
2026-01-08 19:47:43 +0100

    migrate mappings from yarn to official mojang mappings

--- src/main/java/de/arvitus/nospawnerchange/mixin/SpawnEggItemMixin.java
+++ src/main/java/de/arvitus/nospawnerchange/mixin/SpawnEggItemMixin.java
@@ -2,18 +2,18 @@ package de.arvitus.nospawnerchange.mixin;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import de.arvitus.nospawnerchange.config.SpawnerConfig;
-import net.minecraft.block.entity.MobSpawnerBlockEntity;
-import net.minecraft.block.entity.Spawner;
-import net.minecraft.block.entity.TrialSpawnerBlockEntity;
-import net.minecraft.block.pattern.CachedBlockPosition;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemUsageContext;
-import net.minecraft.item.SpawnEggItem;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.random.Random;
-import net.minecraft.world.World;
+import net.minecraft.core.BlockPos;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.SpawnEggItem;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.Spawner;
+import net.minecraft.world.level.block.entity.SpawnerBlockEntity;
+import net.minecraft.world.level.block.entity.TrialSpawnerBlockEntity;
+import net.minecraft.world.level.block.state.pattern.BlockInWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -24,38 +24,38 @@ import static de.arvitus.nospawnerchange.NoSpawnerChange.CONFIG;
 @Mixin(SpawnEggItem.class)
 public class SpawnEggItemMixin {
     @Inject(
-        method = "useOnBlock",
+        method = "useOn",
         at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/item/SpawnEggItem;getEntityType(Lnet/minecraft/item/ItemStack;)" +
-                     "Lnet/minecraft/entity/EntityType;"
+            target = "Lnet/minecraft/world/item/SpawnEggItem;getType(Lnet/minecraft/world/item/ItemStack;)" +
+                     "Lnet/minecraft/world/entity/EntityType;"
         ),
         cancellable = true
     )
     private void disableInSurvival(
-        ItemUsageContext context,
-        CallbackInfoReturnable<ActionResult> cir,
-        @Local World world,
+        UseOnContext context,
+        CallbackInfoReturnable<InteractionResult> cir,
+        @Local Level world,
         @Local ItemStack itemStack,
         @Local BlockPos blockPos,
         @Local Spawner spawner
     ) {
-        PlayerEntity player = context.getPlayer();
+        Player player = context.getPlayer();
         if (player == null || player.isCreative()) return;
 
         SpawnerConfig spawnerConfig = null;
         Boolean isEmpty = null;
 
-        boolean canPlace = itemStack.canPlaceOn(new CachedBlockPosition(world, blockPos, false));
-        if (spawner instanceof MobSpawnerBlockEntity mobSpawner) {
+        boolean canPlace = itemStack.canPlaceOnBlockInAdventureMode(new BlockInWorld(world, blockPos, false));
+        if (spawner instanceof SpawnerBlockEntity mobSpawner) {
             spawnerConfig = CONFIG.monsterSpawner;
-            isEmpty = mobSpawner.getLogic().getRenderedEntity(world, blockPos) == null;
+            isEmpty = mobSpawner.getSpawner().getOrCreateDisplayEntity(world, blockPos) == null;
         } else if (spawner instanceof TrialSpawnerBlockEntity trialSpawner) {
             spawnerConfig = CONFIG.trialSpawner;
             isEmpty = !trialSpawner
-                .getSpawner()
-                .getData()
-                .hasSpawnData(trialSpawner.getSpawner(), Random.create());
+                .getTrialSpawner()
+                .getStateData()
+                .hasMobToSpawn(trialSpawner.getTrialSpawner(), RandomSource.create());
         }
 
         if (
@@ -67,6 +67,6 @@ public class SpawnEggItemMixin {
             return;
         }
 
-        cir.setReturnValue(ActionResult.FAIL);
+        cir.setReturnValue(InteractionResult.FAIL);
     }
 }
